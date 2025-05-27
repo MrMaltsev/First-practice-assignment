b0f40d48a04977217c27fe85dea18ec779c3708d
TakoyKudesnik <84278711+Merkuro256@users.noreply.github.com>
2025-05-27 15:02:49 +0300

    Added 2 and 4

--- src/main/java/pages/BellBeforeFilterSearch.java
+++ src/main/java/pages/BellBeforeFilterSearch.java
@@ -13,15 +13,28 @@ public class BellBeforeFilterSearch {
 
     protected WebDriver chromeDriver;
     protected WebElement specializationDropdown;
+
     protected WebElement hotVacancies;
+
+    protected WebElement locationDropDown;
+
+    protected WebElement onlyDistant;
+
     protected WebDriverWait wait;
     protected WebElement searchButton;
 
     public BellBeforeFilterSearch(WebDriver chromeDriver) {
         this.chromeDriver = chromeDriver;
         this.specializationDropdown = chromeDriver.findElement(By.xpath("//select[@id='edit-field-specializaciya-target-id--2']"));
+
         this.hotVacancies = chromeDriver.findElement(By.xpath("//input[@class='form-check-input' and @id='edit-field-goryachee-value--2']"));
         this.searchButton = chromeDriver.findElement(By.xpath("//div[contains(@class, 'col-md-1 align-self-end')]//button[contains(@class, 'btn btn-danger')]"));
+
+        this.locationDropDown = chromeDriver.findElement(By.xpath("//select[@id='edit-field-lokaciya-target-id--2']"));
+        this.hotVacancies = chromeDriver.findElement(By.xpath("//input[@class='form-check-input' and @id='edit-field-goryachee-value--2']"));
+        this.searchButton = chromeDriver.findElement(By.xpath("//div[contains(@class, 'col-md-1 align-self-end')]//button[contains(@class, 'btn btn-danger')]"));
+        this.onlyDistant = chromeDriver.findElement(By.xpath("//input[@class='form-check-input' and @id='edit-field-udalennka-value--2']"));
+
         this.wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
     }
 
@@ -31,8 +44,23 @@ public class BellBeforeFilterSearch {
         searchButton.click(); // Нажимаем кнопку поиска
     }
 
+
+    public void selectLocation(String location) {
+        Select select = new Select(locationDropDown);
+        select.selectByVisibleText(location);
+        searchButton.click();
+    }
+
+
     public void selectHotVacancies() {
         hotVacancies.click(); // Нажимаем на чекбокс "Горячие вакансии"
         searchButton.click(); // Нажимаем кнопку поиска
     }
+
+
+    public void selectOnlyDistant() {
+        onlyDistant.click();
+        searchButton.click();
+    }
+
 }
